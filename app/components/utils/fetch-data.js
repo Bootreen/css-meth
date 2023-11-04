02dcf1d50e475af9de6411954beedcc58aabad7e
Alex Boot <bootreen@gmail.com>
2023-11-04 15:26:52 +0100

    Changed file location

--- app/components/utils/fetch-data.js
+++ app/components/utils/fetch-data.js
@@ -2,8 +2,8 @@ import fsPromises from 'fs/promises';
 import path from 'path';
 
 const getData = async () => {
-  const descPath = path.join(process.cwd(), '/app/components/bem/bem.dsc');
-  const cssPath = path.join(process.cwd(), '/app/components/bem/bem.css');
+  const descPath = path.join(process.cwd(), '/public/bem.dsc');
+  const cssPath = path.join(process.cwd(), '/public/bem.css');
   const descFile = await fsPromises.readFile(descPath, 'utf8');
   const cssFile = await fsPromises.readFile(cssPath, 'utf8');
   return { desc: descFile, cssSample: cssFile }
